d47bd6e30ec0d4fd70998e10f2fde23076ac5312
CharbelDaccache <charbel270@hotmail.nl>
2017-03-19 22:03:51 +0100

    MIJN OPDRACHTEN

---
+++ Opdracht3Kalender/Opdracht3Kalender/Calender.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calender.aspx.cs" Inherits="Opdracht3Kalender.Calender" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Calendar ID="kalVerjaardag" runat="server" OnSelectionChanged="kalVerjaardag_SelectionChanged"></asp:Calendar>
+    
+    </div>
+        <br />
+        <br />
+        Mijn verjaardag:<br />
+        <asp:Label ID="lblVerjaardag" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        Mijn huidige leeftijd:<br />
+        <asp:Label ID="HuidigeLft" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="LeeftKlik" runat="server" OnClick="LeeftKlik_Click" Text="Calculeer mijn leeftijd" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Opdracht3Kalender/Opdracht3Kalender/Calender.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Opdracht3Kalender
+{
+    public partial class Calender : System.Web.UI.Page
+    {
+        // Dit is een "global" variabele
+        private DateTime verjaardag;
+        private string verjaardagText = "" ;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Dit is geen postback maar de eerste keer dat de webpagina wordt geladen
+                // We zetten de calender op 1 januari 1995
+                
+                 verjaardag = new DateTime(1995, 1, 1);
+                kalVerjaardag.VisibleDate = verjaardag;
+            }
+        }
+    
+
+
+        protected void kalVerjaardag_SelectionChanged(object sender, EventArgs e)
+        {
+             verjaardag = kalVerjaardag.SelectedDate;
+            lblVerjaardag.Text = verjaardagText + verjaardag.ToLongDateString();
+
+            //Bereken nu het aantal jaren dat je oud (jong) bent
+            //Eerst pakken we de huidige datum.
+            DateTime nu = DateTime.Now;
+
+            //We hebben nu de datum van je verjaardag en de hudige datum
+            //We berekenen het verschil
+            TimeSpan verschil = nu.Subtract(verjaardag);
+
+            //Nu nog het aantal jaren berekenen
+            int jaren = verschil.Days / 365;
+        }
+
+        protected void LeeftKlik_Click(object sender, EventArgs e)
+        {
+            // DateTime Time = DateTime.Now;
+            // TimeSpan objSpan = new TimeSpan(36, 0, 0);
+            // DateTime x = Time.Add(objSpan);
+            //lblLeeftijd.Text = Time.ToString();
+
+            String slcDate;
+            slcDate = kalVerjaardag.SelectedDate.ToString("d");
+            int slcYear = Convert.ToInt32(kalVerjaardag.SelectedDate.Year);
+            int crtYear = 2017;
+            int newYear = crtYear - slcYear;
+
+            HuidigeLft.Text = Convert.ToString(newYear);
+            lblVerjaardag.Text = slcDate;
+        }
+    }
+}
