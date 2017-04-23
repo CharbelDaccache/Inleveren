a95a5d3d4cbb92e6beaae42a838e6fdcbeaa565a
CharbelDaccache <charbel270@hotmail.nl>
2017-04-23 21:34:00 +0200

    Opdracht 7 back

---
+++ CharbelBACK7/CharbelBACK7/Toets.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Toets.aspx.cs" Inherits="CharbelBACK7.Toets" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>TafelTester</h1>
+        <p>Vul het hoogste getal in dat gebruikt mag worden in de sommen
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>&nbsp;</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Height="34px" Text="Maak sommen" Width="161px" OnClick="Button1_Click" />
+        </p>
+        <p>&nbsp;</p>
+        <p>Sommen:</p>
+        <p>1 X
+            <asp:Label ID="Som1" runat="server" Text="Label"></asp:Label>
+            =
+            <asp:TextBox ID="TbS1" runat="server"></asp:TextBox>
+            <asp:Label ID="Uit1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>2 X
+            <asp:Label ID="Som2" runat="server" Text="Label"></asp:Label>
+            =
+            <asp:TextBox ID="TbS2" runat="server"></asp:TextBox>
+            <asp:Label ID="Uit2" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>3 X
+            <asp:Label ID="Som3" runat="server" Text="Label"></asp:Label>
+            =
+            <asp:TextBox ID="TbS3" runat="server"></asp:TextBox>
+            <asp:Label ID="Uit3" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>4 X
+            <asp:Label ID="Som4" runat="server" Text="Label"></asp:Label>
+            =
+            <asp:TextBox ID="TbS4" runat="server"></asp:TextBox>
+            <asp:Label ID="Uit4" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>5 X
+            <asp:Label ID="Som5" runat="server" Text="Label"></asp:Label>
+            =
+            <asp:TextBox ID="TbS5" runat="server"></asp:TextBox>
+            <asp:Label ID="Uit5" runat="server" Text="Label"></asp:Label>
+        </p>
+        </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Height="34px" Text="Calculeer cijfer" Width="161px" OnClick="Button2_Click" />
+        <br />
+        <br />
+        Je hebt een: <asp:Label ID="Scorelbl" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ CharbelBACK7/CharbelBACK7/Toets.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CharbelBACK7
+{
+    public partial class Toets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int hg = Convert.ToInt32(TextBox1.Text);
+
+            Random generator = new Random();
+            int Getal1 = generator.Next(hg) + 1;
+
+            Random generator1 = new Random();
+            int Getal2 = generator.Next(hg) + 1;
+
+            Random generator2 = new Random();
+            int Getal3 = generator.Next(hg) + 1;
+
+            Random generator3 = new Random();
+            int Getal4 = generator.Next(hg) + 1;
+
+            Random generator4 = new Random();
+            int Getal5 = generator.Next(hg) + 1;
+
+
+
+
+            Som1.Text = Getal1.ToString();
+            Som2.Text = Getal2.ToString();
+            Som3.Text = Getal3.ToString();
+            Som4.Text = Getal4.ToString();
+            Som5.Text = Getal5.ToString();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int Score = 10;
+            if (TbS1.Text == Convert.ToString(Convert.ToInt16(Som1.Text) * 1) )
+            {
+                Uit1.Text = "Goed";
+            }
+
+            else
+            {
+                Uit1.Text = "Fout";
+                Score = Score - 2;
+               
+            }
+
+
+            if (TbS2.Text == Convert.ToString(Convert.ToInt16(Som2.Text) * 2))
+            {
+                Uit2.Text = "Goed";
+            }
+
+            else
+            {
+                Uit2.Text = "Fout";
+                Score = Score - 2;
+            }
+
+            if (TbS3.Text == Convert.ToString(Convert.ToInt16(Som3.Text) * 3))
+            {
+                Uit3.Text = "Goed";
+            }
+
+            else
+            {
+                Uit3.Text = "Fout";
+                Score = Score - 2;
+            }
+
+            if (TbS4.Text == Convert.ToString(Convert.ToInt16(Som4.Text) * 4))
+            {
+                Uit4.Text = "Goed";
+            }
+
+            else
+            {
+                Uit4.Text = "Fout";
+                Score = Score - 2;
+            }
+
+            if (TbS5.Text == Convert.ToString(Convert.ToInt16(Som5.Text) * 5))
+            {
+                Uit5.Text = "Goed";
+            }
+
+            else
+            {
+                Uit5.Text = "Fout";
+                Score = Score - 2;
+            }
+            Scorelbl.Text =  Convert.ToString(Score);
+
+
+        }
+    }
+}
